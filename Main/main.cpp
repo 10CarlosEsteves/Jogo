1ed4b5cebeccf8c15bdbf51de7ff6e327012d805
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2022-11-27 13:28:23 -0300

    Create main.cpp

---
+++ Main/main.cpp
@@ -0,0 +1,24 @@
+#include <conio.h>
+#include <iostream>
+#include <stdlib.h>
+
+#include "UI.h"
+#include "Personagem.h"
+ 
+
+using namespace std;
+
+int main(){
+	
+	char escolha;
+	Interface interface;
+	
+	escolha = interface.Main_Menu();
+	
+	
+	
+	
+	
+
+	return 0;
+}
