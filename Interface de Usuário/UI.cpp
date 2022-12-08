f2d0dd098e1108410b185f2175666d4e91a8148b
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2022-12-08 16:04:57 -0300

    Create UI.cpp

---
+++ Interface de Usuário/UI.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "UI.h"
+
+using namespace std;
+
+namespace Interface{
+
+	void Main_Menu(){
+        cout<<"============================="<<endl;
+		cout<<"            MENU             "<<endl;
+		cout<<"============================="<<endl;
+		cout<<"[1]     INICIAR CAMPANHA     "<<endl;
+		cout<<"[2]     CONTINUAR CAMPANHA   "<<endl;
+		cout<<"[3]         AJUDA            "<<endl;
+		cout<<"[4]         SAIR             "<<endl;
+		cout<<"============================="<<endl;
+    }
+    
+    
+}
