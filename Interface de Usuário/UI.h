baf2c967bbe9729a7dc2d492fe8981091839ecd8
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2022-12-08 16:05:18 -0300

    Create UI.h

---
+++ Interface de Usuário/UI.h
@@ -0,0 +1,17 @@
+#ifndef USER_INTERFACE_H
+#define USER_INTERFACE_H
+
+#include <conio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+namespace Interface{
+
+	//Função da Interface do Menu Principal.
+    void Main_Menu();
+
+}
+
+#endif
